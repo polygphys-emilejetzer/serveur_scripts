74b5bd666cc90d2e48b1677201a801404362b1f3
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-09-20 15:53:18 -0400

    Ajout de certains scripts

--- scripts/admin/exporter_heures.py
+++ scripts/admin/exporter_heures.py
@@ -13,7 +13,6 @@ import schedule
 
 import pandas as pd
 
-from polygphys.serveur.racine.canari import créer_journal, noter_exceptions
 from polygphys.outils.reseau import DisqueRéseau
 from polygphys.outils.journal import Repository
 
@@ -23,7 +22,6 @@ from polygphys.admin.heures.heures import FeuilleDeTempsConfig, FeuilleDeTemps
 def exporter(d: DisqueRéseau,
              config: FeuilleDeTempsConfig,
              feuille_de_temps: FeuilleDeTemps):
-    journal.info('Montage du disque au point %s', d.chemin)
     git = Repository(d.chemin)
 
     fichier_excel = next(d.chemin.glob(config.get('export', 'fichier')))
@@ -68,37 +66,24 @@ def exporter(d: DisqueRéseau,
 
 def charger_config(config: FeuilleDeTempsConfig):
     adresse = config.get('bd', 'adresse')
-    journal.debug('Adresse de la base de données: %s', adresse)
 
     feuille_de_temps = FeuilleDeTemps(adresse)
-    journal.debug('Objet «feuille de temps»: %s', feuille_de_temps)
 
     url = config.get('export', 'disque')
     journal.debug('Disque d\'exportation: %s', url)
 
     chemin = Path(config.get('export', 'montage')).expanduser()
-    journal.debug('Chemin de montage: %s', chemin)
 
     nom = config.get('export', 'nom')
-    journal.debug('Nom de connexion: %s', nom)
 
     journal.info('Obtention du mot de passe du disque réseau...')
     mdp = keyring.get_password('system', f'exporter_heures_{nom}')
     if mdp is None:
         mdp = getpass.getpass('mdp: ')
         keyring.set_password('system', f'exporter_heures_{nom}', mdp)
-    journal.info('Mot de passe obtenu et sauvegardé.')
 
     return url, chemin, nom, mdp, feuille_de_temps
 
-# Script
-
-
-journal = créer_journal(__name__, __file__)
-horaire = schedule.every(10).minutes
-
-
-@noter_exceptions(journal)
 def main():
     chemin = Path('~/Documents/Polytechnique/Heures').expanduser()
     config = FeuilleDeTempsConfig(chemin / 'heures.cfg')
@@ -106,20 +91,5 @@ def main():
     with DisqueRéseau(url, chemin, 'J', nom, mdp) as d:
         exporter(d, config, feuille_de_temps)
 
-
-def html(chemin: Path):
-    with chemin.open('w') as fichier:
-        print('<html>',
-              '    <head>',
-              f'        <title>{__name__} à {__file__}</title>',
-              '    </head>',
-              '    <body>',
-              f'        <h1>{__name__} à {__file__}</h1>',
-              f'        <p>Fonctionnel à {datetime.isoformat(datetime.now())}.</p>',
-              '        <hr/>',
-              f'        <h1>{chemin.with_suffix(".log")}</h1>',
-              f'        <pre>{chemin.with_suffix(".log").open("r").read()}</pre>',
-              '    </body>',
-              '</html>',
-              sep='\n',
-              file=fichier)
+if __name__ == '__main__':
+    main()
