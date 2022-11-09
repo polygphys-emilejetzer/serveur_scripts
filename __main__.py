c2bc54a635b0318b8b9e45c2d5105e6128ca6d4f
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-11-09 17:10:30 -0500

    Exécution comme module

--- __main__.py
+++ __main__.py
@@ -15,6 +15,30 @@ lecteur_arguments = argparse.ArgumentParser(
     epilog='Contacter Émile Jetzer @ Polytechnique Montréal pour plus de détails.')
 lecteur_arguments.add_argument(
     '-x', '--config', dest='config', default=str(CONFIG), required=False)
+lecteur_arguments.add_argument(
+    '-i', '--init', dest='init', default=False, required=False)
+
+
 arguments = lecteur_arguments.parse_args()
 
-main(arguments.config)
+if lecteur_arguments.init:
+    config = lecteur_arguments.config
+    if not config.exists():
+        with CONFIG.open('r', encoding='utf-8') as original:
+            with open(config, 'w', encoding='utf-8') as nouveau:
+                nouveau.write(original.read())
+
+    répertoire_journaux = Path('./journaux/')
+    répertoire_modèles = Path('./modèles/')
+    répertoire_config = Path('./config/')
+
+    if not répertoire_journaux.exists():
+        répertoire_journaux.mkdir()
+    if not répertoire_modèles.exists():
+        répertoire_modèles.mkdir()
+    if not répertoire_config.exists():
+        répertoire_config.mkdir()
+
+    print('Il faut placer les scripts correctement dans les bons dossiers...')
+else:
+    main(arguments.config)
--- nouveau_script.py
+++ nouveau_script.py
@@ -6,68 +6,75 @@ from pathlib import Path
 from polygphys.serveur import WebScriptConfig
 from polygphys.outils.journal import Repository
 
-répertoire_journaux = Path('journaux/')
-répertoire_modèles = Path('modèles/')
-répertoire_config = Path('config/')
 
-# Configuration de base
-print('[script]')
-nom_de_script = input('nom> ')
-chemin_de_script = input('chemin (relatif)> ')
-fonction_à_exécuter = input('fonction> ')
+def main():
+    répertoire_journaux = Path('./journaux/')
+    répertoire_modèles = Path('./modèles/')
+    répertoire_config = Path('./config/')
 
-chemin_config = (répertoire_config / nom_de_script).with_suffix('.cfg')
-script_config = WebScriptConfig(chemin_config)
-script_config.set('script', 'nom', nom_de_script)
-script_config.set('script', 'chemin', chemin_de_script)
-script_config.set('script', 'fonction', fonction_à_exécuter)
+    # Configuration de base
+    print('[script]')
+    nom_de_script = input('nom> ')
+    chemin_de_script = input('chemin (relatif)> ')
+    fonction_à_exécuter = input('fonction> ')
 
-# Journalisation
-print('[journal]')
+    chemin_config = (répertoire_config / nom_de_script).with_suffix('.cfg')
+    script_config = WebScriptConfig(chemin_config)
+    script_config.set('script', 'nom', nom_de_script)
+    script_config.set('script', 'chemin', chemin_de_script)
+    script_config.set('script', 'fonction', fonction_à_exécuter)
 
-répertoire_journal = répertoire_journaux / nom_de_script
-chemin_journal = (répertoire_journal / nom_de_script).with_suffix('.sqlite')
-adresse = f'sqlite:///{chemin_journal}'
+    # Journalisation
+    print('[journal]')
 
-script_config.set('journal', 'adresse', str(adresse))
-script_config.set('journal', 'répertoire', str(répertoire_journal))
+    répertoire_journal = répertoire_journaux / nom_de_script
+    chemin_journal = (répertoire_journal /
+                      nom_de_script).with_suffix('.sqlite')
+    adresse = f'sqlite:///{chemin_journal}'
 
-répertoire_journal.mkdir()
-chemin_journal.touch()
+    script_config.set('journal', 'adresse', str(adresse))
+    script_config.set('journal', 'répertoire', str(répertoire_journal))
 
-git = Repository(répertoire_journal)
-git.init()
-git.add(chemin_journal.name)
-git.commit('Premier commit.')
+    répertoire_journal.mkdir()
+    chemin_journal.touch()
 
-# Modèle HTML
-print('[html]')
-print('modèles disponibles:')
-for m in répertoire_modèles.glob('*.html'):
-    print(m.name)
-nom_modèle = input('nom du fichier> ')
+    git = Repository(répertoire_journal)
+    git.init()
+    git.add(chemin_journal.name)
+    git.commit('Premier commit.')
 
-chemin_modèle = répertoire_modèles / nom_modèle
+    # Modèle HTML
+    print('[html]')
+    print('modèles disponibles:')
+    for m in répertoire_modèles.glob('*.html'):
+        print(m.name)
+    nom_modèle = input('nom du fichier> ')
 
-script_config.set('html', 'modèle', str(chemin_modèle))
+    chemin_modèle = répertoire_modèles / nom_modèle
 
-# Programmation horaire
-print('[horaire]')
-intervalle = input('intervalle> ')
-unité = input('nom de fonction d\'unité du module schedule> ')
-à = input('moment dans l\'intervalle pour l\'exécution> ')
+    script_config.set('html', 'modèle', str(chemin_modèle))
 
-if intervalle == '':
-    intervalle = 1
-else:
-    intervalle = int(intervalle)
+    # Programmation horaire
+    print('[horaire]')
+    intervalle = input('intervalle> ')
+    unité = input('nom de fonction d\'unité du module schedule> ')
+    à = input('moment dans l\'intervalle pour l\'exécution> ')
 
-if unité == '':
-    unité = 'hours'
+    if intervalle == '':
+        intervalle = 1
+    else:
+        intervalle = int(intervalle)
 
-if à == '':
-    à = ':00'
+    if unité == '':
+        unité = 'hours'
 
-script_config.set('horaire', 'intervalle', str(intervalle))
-script_config.set('horaire', 'unité', unité)
-script_config.set('horaire', 'à', à)
+    if à == '':
+        à = ':00'
+
+    script_config.set('horaire', 'intervalle', str(intervalle))
+    script_config.set('horaire', 'unité', unité)
+    script_config.set('horaire', 'à', à)
+
+
+if __name__ == '__main__':
+    main()
