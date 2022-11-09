f1ab4bee0c18b23e9fe2caabfddca32f6fe0f053
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-11-09 16:35:51 -0500

    Exécution comme module

--- __main__.py
+++ __main__.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 """Créé le Mon Sep 19 09:06:13 2022 par emilejetzer."""
 
+import argparse
+
+from pathlib import Path
+
 from . import main
 
-main()
+CONFIG = Path(__file__).parent / 'serveur.cfg'
+
+lecteur_arguments = argparse.ArgumentParser(
+    prog='Serveur de scripts horaires',
+    description='Exécuter des scripts périodiquement et obtenir leur statut.',
+    epilog='Contacter Émile Jetzer @ Polytechnique Montréal pour plus de détails.')
+lecteur_arguments.add_argument('config', default=str(CONFIG))
+arguments = lecteur_arguments.parse_args()
+
+main(arguments.config)
