2ced1f1a80cf653dc1193ee88cc79bb476152194
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-11-09 16:37:47 -0500

    Exécution comme module

--- __main__.py
+++ __main__.py
@@ -13,7 +13,7 @@ lecteur_arguments = argparse.ArgumentParser(
     prog='Serveur de scripts horaires',
     description='Exécuter des scripts périodiquement et obtenir leur statut.',
     epilog='Contacter Émile Jetzer @ Polytechnique Montréal pour plus de détails.')
-lecteur_arguments.add_argument('config', default=str(CONFIG))
+lecteur_arguments.add_argument('config', default=str(CONFIG), required=False)
 arguments = lecteur_arguments.parse_args()
 
 main(arguments.config)
